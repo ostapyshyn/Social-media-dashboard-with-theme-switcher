346f44d0b269db35ed7d34cf52cff92a8cb4b898
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2023-05-18 23:07:10 +0300

    Add styles for card

--- src/components/SocialCard/SocialCard.tsx
+++ src/components/SocialCard/SocialCard.tsx
@@ -2,9 +2,11 @@ import styles from './SocialCard.module.scss'
 
 const SocialCard = () => {
   return (
-    <section>
-      <h2>1987</h2>
-      <p>FOLLOWERS</p>
+    <section className={styles.card}>
+      <p className={styles.user}>@nathanf</p>
+      <h2 className={styles.followers}>1987</h2>
+      <p className={styles.description}>FOLLOWERS</p>
+      <p className={styles.result}>12 Today</p>
     </section>
   )
 }
