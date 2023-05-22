3de460b240081f83f89460139f086e22b0f21623
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2023-05-22 22:35:14 +0300

    Add data files

---
+++ src/assets/dayliCards.ts
@@ -0,0 +1,76 @@
+import facebookImg from '../assets/icon-facebook.svg'
+import twitterImg from '../assets/icon-twitter.svg'
+import instagramImg from '../assets/icon-instagram.svg'
+import youtubeImg from '../assets/icon-youtube.svg'
+import iconUp from '../assets/icon-up.svg'
+import iconDown from '../assets/icon-down.svg'
+
+const secondaryCards = [
+  {
+    id: 1,
+    title: 'Page Views',
+    percentage: '3%',
+    image: facebookImg,
+    icon: iconUp,
+    target: 87,
+  },
+  {
+    id: 2,
+    title: 'Likes',
+    percentage: '2%',
+    image: facebookImg,
+    icon: iconDown,
+    target: 52,
+  },
+  {
+    id: 3,
+    title: 'Likes',
+    percentage: '2257%',
+    image: instagramImg,
+    icon: iconUp,
+    target: 5462,
+  },
+  {
+    id: 4,
+    title: 'Profile Views',
+    percentage: '2257%',
+    image: instagramImg,
+    icon: iconUp,
+    target: 52,
+    letter: 'k',
+  },
+  {
+    id: 5,
+    title: 'Retweets',
+    percentage: '303%',
+    image: twitterImg,
+    icon: iconUp,
+    target: 117,
+  },
+  {
+    id: 6,
+    title: 'Likes',
+    percentage: '553%',
+    image: twitterImg,
+    icon: iconUp,
+    target: 507,
+  },
+  {
+    id: 7,
+    title: 'Likes',
+    percentage: '19%',
+    image: youtubeImg,
+    icon: iconDown,
+    target: 107,
+  },
+  {
+    id: 8,
+    title: 'Total Views',
+    percentage: '12%',
+    image: youtubeImg,
+    icon: iconDown,
+    target: 1407,
+  },
+]
+
+export default secondaryCards
---
+++ src/assets/mainCards.ts
@@ -0,0 +1,48 @@
+import facebookImg from '../assets/icon-facebook.svg'
+import twitterImg from '../assets/icon-twitter.svg'
+import instagramImg from '../assets/icon-instagram.svg'
+import youtubeImg from '../assets/icon-youtube.svg'
+import iconUp from '../assets/icon-up.svg'
+import iconDown from '../assets/icon-down.svg'
+
+const mainCards = [
+  {
+    id: 1,
+    userName: '@nathanf',
+    followText: 'followers',
+    followToday: '12 Today',
+    image: facebookImg,
+    icon: iconUp,
+    target: 1987,
+  },
+  {
+    id: 2,
+    userName: '@nathanf',
+    followText: 'followers',
+    followToday: '99 Today',
+    image: twitterImg,
+    icon: iconUp,
+    target: 1044,
+  },
+  {
+    id: 3,
+    userName: '@realnathanf',
+    followText: 'followers',
+    followToday: '1099 Today',
+    image: instagramImg,
+    icon: iconUp,
+    target: 11,
+    letter: 'k',
+  },
+  {
+    id: 4,
+    userName: 'Nathan F.',
+    followText: 'subscribers',
+    followToday: '144 Today',
+    image: youtubeImg,
+    icon: iconDown,
+    target: 8239,
+  },
+]
+
+export default mainCards
