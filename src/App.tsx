eb8b54e6c535d515b5c2a9e6424aa19b2fb9b2f6
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2023-05-25 13:29:59 +0300

    Add columns

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,26 @@ import styles from './App.module.scss'
 import DayCard from './components/DayCard'
 import Header from './components/Header'
 import SocialCard from './components/SocialCard'
+import mainCards from './assets/mainCards'
+import dayliCards from './assets/dayliCards'
 
 function App() {
   return (
     <main className={styles.App}>
       <Header />
-      <SocialCard />
+      <section className={styles.cards}>
+        {mainCards.map((card) => (
+          <SocialCard key={card.id} {...card} />
+        ))}
+      </section>
+
       <h2>Overview - Today</h2>
-      <DayCard />
+
+      <section className={styles.dayCards}>
+        {dayliCards.map((card) => (
+          <DayCard key={card.id} {...card} />
+        ))}
+      </section>
     </main>
   )
 }
